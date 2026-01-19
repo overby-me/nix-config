a3d9704cbfcc2b2f2efa1a47df8f30fb15b045df
Niclas Overby <niclas@overby.me>
2026-01-19 10:33:55 +0100

    fix(nix/config): Fix nix flake check

--- configurations/home-manager/noverby.nix
+++ configurations/home-manager/noverby.nix
@@ -1,7 +1,11 @@
-{inputs, ...}: {
+{
+  inputs,
+  src,
+  ...
+}: {
   system = "x86_64-linux";
   extraSpecialArgs = {
-    inherit inputs;
+    inherit inputs src;
     stateVersion = "24.05";
   };
   modules = [inputs.self.homeModules.noverby];
