16e0b2f5c78abcd587de841664cec9d496ac458e
Niclas Overby <niclas@overby.me>
2026-01-28 11:22:23 +0100

    chore(nix/config): Add overby-me cachix binary cache

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -7,11 +7,13 @@
       experimental-features = "nix-command flakes ca-derivations";
       download-buffer-size = 1024 * 1024 * 1024;
       substituters = [
+        "https://overby-me.cachix.org"
         "https://nix-community.cachix.org"
         "https://zed.cachix.org"
         "https://cache.garnix.io"
       ];
       trusted-public-keys = [
+        "overby-me.cachix.org-1:dU7qOj5u97QZz98nqnh+Nwait6c+2d2Eq0KTOAXTyp4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
