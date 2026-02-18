628525919e8a79e17d9d5f294a9db7dff8ab699c
Niclas Overby <niclas@overby.me>
2026-02-18 01:08:56 +0100

    fix(nix/config): Use tspin-pager wrapper to auto-detect interactive terminal

--- modules/nixos/core/environment.nix
+++ modules/nixos/core/environment.nix
@@ -1,11 +1,20 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  tspin-pager = pkgs.writeShellScriptBin "tspin-pager" ''
+    if [ -t 1 ]; then
+      exec ${pkgs.tailspin}/bin/tspin "$@"
+    else
+      exec ${pkgs.tailspin}/bin/tspin --print "$@"
+    fi
+  '';
+in {
   environment = {
     systemPackages = with pkgs; [
       helix
       tailspin
+      tspin-pager
     ];
     sessionVariables = {
-      PAGER = "tspin --print";
+      PAGER = "tspin-pager";
       SYSTEMD_PAGERSECURE = "1";
       NIXOS_OZONE_WL = "1";
     };
