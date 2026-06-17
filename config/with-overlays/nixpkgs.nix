9ee307c0e993d63f0dff38981d092a369f35ba8d
Niclas Overby <niclas@overby.me>
2026-06-17 14:36:46 +0200

    chore(flake.nix): Move nix subtree to repository root

--- config/with-overlays/nixpkgs.nix
+++
@@ -1,9 +0,0 @@
-(final: prev: {
-  # Add unstable packages to attributeset to pkgs.pkgsUnstable
-  pkgsUnstable =
-    (import prev.inputs.nixpkgs-unstable {
-      inherit (final.stdenv.hostPlatform) system;
-      config.allowUnfree = true;
-    })
-    // prev.inputs.self.packages.${final.stdenv.hostPlatform.system};
-})
